e6909774667248addff81d5508d1ae23b70ac755
Bryant-Wang24 <485969746@qq.com>
2021-01-14 23:16:46 +0800

    update

--- src/x.js
+++ src/x.js
@@ -1,1 +1,2 @@
-console.log(xxx);
+import './x.css'
+export default 'xxx'
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
+const { title } = require('process');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         filename: '[name].[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: '王秋爽',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     }
 };
